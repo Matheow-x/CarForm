32586972cbb2ca7dfaa666c86f9e33310d6e5c66
Matheow-x <mehdigardshasan0@gmail.com>
2026-09-17 19:48:17 +0330

    Delete CarForm directory

--- CarForm/Models/Setting.cs
+++
@@ -1,9 +0,0 @@
-namespace CarForm.Models;
-
-/// <summary>Simple key/value store for application and print settings (single-user app).</summary>
-public class Setting
-{
-    public string Key { get; set; } = string.Empty;
-
-    public string? Value { get; set; }
-}
--- CarForm/Printing/FormCell.cs
+++
@@ -1,4 +0,0 @@
-namespace CarForm.Printing;
-
-/// <summary>One labeled field of the printed form.</summary>
-public readonly record struct FormCell(string Label, string Value, int ColumnSpan = 1, bool Emphasis = false);
